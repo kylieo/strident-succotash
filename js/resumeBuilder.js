1905eedfc1ac21ed81ef80f2d436965b6fa5201d
Kylie O'Brien-Pratt <Kylie@Kylies-MacBook-Air.local>
2015-12-01 00:46:27 +1100

    Edit template name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
-var formattedName = "Kylie O'Brien-Pratt";
+var name = "Kylie O'Brien-Pratt";
 
-var formatterRole = "Web Developer | Town Planner | Data Enthusiast";
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Web Developer | Town Planner | Data Enthusiast";
+
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$('#header').prepend(formattedName);
+
+$('#header').prepend(formattedRole);
